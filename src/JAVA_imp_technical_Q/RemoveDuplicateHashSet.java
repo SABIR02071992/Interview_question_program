56cc7a99a34a11fbe795fbb92c90e80533b1d7ea
Sabir_work <shabiransari671@gmail.com>
2025-10-22 17:39:59 +0530

    SortAndRemoveDuplicates__1

--- src/JAVA_imp_technical_Q/RemoveDuplicateHashSet.java
+++ src/JAVA_imp_technical_Q/RemoveDuplicateHashSet.java
@@ -1,34 +1,21 @@
 package JAVA_imp_technical_Q;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+
 
 public class RemoveDuplicateHashSet {
     public static void main(String[] args) {
-        int[]a = {1,2,3,3,4,4,10,6};
-        int temp;
-
-        for (int i = 0; i<a.length;i++){
-
-            for (int j = i+1; j<a.length;j++){
-                if (a[i]>a[j]){
-                    temp = a[i];
-                    a[i]= a[j];
-                    a[j]= temp;
-
-                }
-            }
-
-        }
-        for (int i=0; i<a.length;i++){
-            System.out.println(a[i]);
-        }
+        int[]a = {1,2,8,3,3,4,4};
 
         //HashSet<Integer> h = new HashSet<>();
+        LinkedHashSet<Integer>h = new LinkedHashSet<>();
+        // if use LinkedHashSet then return same array list with remove duplicate element
 
-        /*for (int i=0; i<a.length;i++){
+        for (int i=0; i<a.length;i++){
             h.add(a[i]);
-        }*/
-        //System.out.println("dddd"+h);
+        }
+        System.out.println("dddd"+h);
 
     }
 }
